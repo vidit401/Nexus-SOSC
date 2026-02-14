f264189e246078cd1d047425f6633ac4c654d755
Vidit Bansal <79268931+vidit401@users.noreply.github.com>
2026-02-14 18:12:13 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+let events = []
+
+function addEvent() {
+  let name = document.getElementById("eventName").value
+  let date = document.getElementById("eventDate").value
+
+  if (name === "" || date === "") {
+    alert("Please fill all fields")
+    return
+  }
+
+  events.push({ name, date })
+  renderEvents()
+}
+
+function renderEvents() {
+  let list = document.getElementById("eventList")
+  list.innerHTML = ""
+
+  events.forEach((event, index) => {
+    let li = document.createElement("li")
+    li.innerHTML = `${event.name} - ${event.date} 
+      <button onclick="deleteEvent(${index})">Delete</button>`
+    list.appendChild(li)
+  })
+}
+
+function deleteEvent(index) {
+  events.splice(index, 1)
+  renderEvents()
+}
